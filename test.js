c0cac9e18f7e93b3a1c406a64a68c1733042bf55
Woody Anderson <woody.anderson@gmail.com>
2011-06-10 00:48:08 -0700

    test.js was old

--- test.js
+++ test.js
@@ -1,38 +1,42 @@
-var compress=require("./compress");
-var sys=require("sys");
-var posix=require("posix");
+var gzbz2 = require("./gzbz2");
+var sys = require("sys");
+var fs = require("fs");
+
+// Read in our test file
+var testfile = process.argv[2] || "filetest.js";
+var enc = process.argv[3];
+var data = fs.readFileSync(testfile, enc);
+sys.puts("Got : " + data.length);
+
+// Set output file
+var fd = fs.openSync(testfile + ".gz", "w", 0644);
+sys.puts("File opened");
 
 // Create gzip stream
-var gzip=new compress.Gzip;
+var gzip = new gzbz2.Gzip;
 gzip.init();
 
-// Pump data to be compressed
-var gzdata1 = gzip.deflate("My data that needs ", "binary"); 
-sys.puts("Compressed size : "+gzdata1.length);
+// Pump data to be gzbz2
+var gzdata = gzip.deflate(data, enc);  // Do this as many times as required
+sys.puts("Compressed chunk size : " + gzdata.length);
+fs.writeSync(fd, gzdata, 0, gzdata.length, null);
 
-var gzdata2 = gzip.deflate("to be compressed. 01234567890.", "binary"); 
-sys.puts("Compressed size : "+gzdata2.length);
+// Get the last bit
+var gzlast = gzip.end();
+sys.puts("Compressed chunk size: " + gzlast.length);
+fs.writeSync(fd, gzlast, 0, gzlast.length, null);
+fs.closeSync(fd);
+sys.puts("File closed");
 
-var gzdata3=gzip.end();
-sys.puts("Last bit : "+gzdata3.length);
+// See if we can uncompress it ok
+var gunzip = new gzbz2.Gunzip;
+gunzip.init({encoding: enc});
+var testdata = fs.readFileSync(testfile + ".gz");
+sys.puts("Test opened : " + testdata.length);
+var inflated = gunzip.inflate(testdata, enc);
+sys.puts("GZ.inflate.length: " + inflated.length);
+gunzip.end(); // no return value
 
-// Take the output stream, and chop it up into two
-var gzdata = gzdata1+gzdata2+gzdata3;
-sys.puts("Total compressed size : "+gzdata.length);
-var d1 = gzdata.substr(0, 25);
-var d2 = gzdata.substr(25);
-
-// Create gunzip stream to decode these
-var gunzip = new compress.Gunzip;
-gunzip.init();
-var data1 = gunzip.inflate(d1, "binary");
-var data2 = gunzip.inflate(d2, "binary");
-var data3 = gunzip.end();
-
-sys.puts(data1+data2+data3);
-
-
-
-
-
-
+if (data.length != inflated.length) {
+    sys.puts('error! input/output string lengths do not match');
+}
